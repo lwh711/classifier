d28d8f9e3c493a3b2bef2f2cfd7f019fdd554169
lwh711 <75018138+lwh711@users.noreply.github.com>
2020-11-26 14:59:56 +0800

    注释掉几行代码

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #encoding:utf-8
 # print('这是一个demo，测试git push 及 pull')
 # 从服务器修改了代码
-print("修改了但没提交")
-print("remote修改了代码")
-print("本地没有修改怎么办？")
+#print("修改了但没提交")
+#print("remote修改了代码")
+#print("本地没有修改怎么办？")
 print("好了好了，本地和服务器都合并啦")
