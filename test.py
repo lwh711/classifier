ea6f729cb2983f5e515cfbd8f6583a56fdd2d4a7
lwh711 <75018138+lwh711@users.noreply.github.com>
2020-12-28 14:47:31 +0800

    Delete test.py
    服务器删除文件，测试本地上传是否会重新上传

--- test.py
+++
@@ -1,9 +0,0 @@
-#encoding:utf-8
-# print('这是一个demo，测试git push 及 pull')
-# 从服务器修改了代码
-#print("修改了但没提交")
-#print("remote修改了代码")
-#print("本地没有修改怎么办？")
-print("好了好了，本地和服务器都合并啦")
-print('有点奇怪，push 不上去')
-print('再次调整test.py，看本地及服务器是否一致')
