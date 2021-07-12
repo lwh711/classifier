881902cde775746455ed342f8ddf1985a5ebd0d3
bidi <382030701@qq.com>
2021-07-12 16:07:56 +0800

    修改文件

--- new_test.py
+++ new_test.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Author  : bidikeji
 # @Time    : 2020/12/28 0028 14:48 
-print('新增文件')
+# print('新增文件')
 print("有点奇怪，push提示成功了，但服务器没有这个文件")
 print('为什么没有这个文件，也不能增加，push')
+print('merge change')
